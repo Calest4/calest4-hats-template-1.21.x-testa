407a2fc7c655564a3dae6e688e504747fd7c9b1c
Calest4 <mileinacelesteria352@gmail.com>
2024-11-09 01:16:39 +0700

    test push to git

--- src/main/java/sol/calest4/hat/Calest4Hats.java
+++ src/main/java/sol/calest4/hat/Calest4Hats.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//gonna change this?
 public class Calest4Hats implements ModInitializer {
 	public static final String MOD_ID = "calest4-hats";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
